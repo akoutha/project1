1a9f488c7290d41f5c8d3d68c774d20c6a8fb5fa
Abishek Koutha <akoutha7@gmail.com>
2020-02-28 16:15:43 -0500

    final touches

--- client.py
+++ client.py
@@ -7,54 +7,71 @@ import socket
 
 def client():
 
-        connectTS = 0
+
+        hostname = sys.argv[1] #hostname
+        connectTS = 0 #check to see if TS server is connected or not
+        tsHostname = ""
         try:
             cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             print("[C]: Client socket created")
         except socket.error as err:
             print('socket open error: {} \n'.format(err))
             exit()
-            #rsHostname = sys.argv[1]
 
-        localhost_addr = socket.gethostbyname(socket.gethostname())
+
+        if sys.argv[1] == "local":
+            localhost_addr = socket.gethostbyname(socket.gethostname())
+            print "got local addy"
+        else:
+            localhost_addr = socket.gethostbyname(hostname)
+
+
+
         server_binding = (localhost_addr, 50788) #add rsPort
         cs.connect(server_binding)
 
         with open("PROJI-HNS.txt") as fp:
-            line = fp.readline()
-            line = line.strip()
+            line = fp.readline() #get first line
+            line = line.strip() #remove spaces
             file = open("test.txt","w+")
             while line:
                 print "This line is: " + line
-                cs.sendall(line)
+                cs.sendall(line) #send to rs
                 data_from_server=cs.recv(1024) # Receive data from the server
                 print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
                 dataList = data_from_server.split()  #split data from server
 
                 print dataList
 
-                if dataList[2] == "A":
+                if dataList[2] == "A": #if available
                     file.write(data_from_server+"\n")
                     print "wrote onto file - RS"
-                else: #check TS
+                else: #check TS if not available
                     print "no match in rs table, checking TS table"
-                    if connectTS == 0:
-                        print "intial TS connect"
-                        connectTS = connectTs(50789)
+                    tsHostname = dataList[0]
 
-                    connectTS.sendall(line)
-                    data_from_server = connectTS.recv(1024)
+                    if dataList[0] == "localhost":
+                        tsHostname = socket.gethostname()
+                    else:
+                        tsHostname = dataList[0]
+
+                    if connectTS == 0: #initial connection to TS
+                        print "intial TS connect"
+                        connectTS = connectTs(tsHostname)
+
+                    connectTS.sendall(line) #send to ts
+                    data_from_server = connectTS.recv(1024) #recv from ts
                     print("[C]: Data received from TS server: {}".format(data_from_server.decode('utf-8')))
                     dataList = data_from_server.split()
 
                     if dataList[2] == "A":
                         file.write(data_from_server+"\n")
                         print "wrote onto file - TS"
-                    else:
+                    else: #not found in any server
                         print "No match in TS - ERROR"
                         file.write(data_from_server+"\n")
 
-                line = fp.readline()
+                line = fp.readline() #move next line
 
         cs.close()
         connectTS.close()
@@ -71,8 +88,8 @@ def connectTs(host):
         print('socket open error: {} \n'.format(err))
     #tsListenPort = int(sys.argv[3])
     #port = tsListenPort
-    localhost_addr = socket.gethostbyname(socket.gethostname())
-    #localhost_addr = socket.gethostbyname(host)
+    #localhost_addr = socket.gethostbyname(socket.gethostname())
+    localhost_addr = socket.gethostbyname(host)
 
     # connect to the server on local machine
     server_binding = (localhost_addr, 50789)
